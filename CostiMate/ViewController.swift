cb0481cc4c1fcf29037fde1d39fb2c0bb23a792f
Max Huang <github@maxhuang.nz>
2019-02-16 12:59:31 +1300

    Added Google Mobile Ads SDK and implemented simple banner ad.

--- CostiMate/ViewController.swift
+++ CostiMate/ViewController.swift
@@ -7,15 +7,24 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var bannerView: GADBannerView!
     
     var expenses: [Expense] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
+        
+        
         
         expenses = createArray()
         
---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'CostiMate' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for CostiMate
+  pod 'Google-Mobile-Ads-SDK'
+  pod 'Firebase/Core'
+  pod 'Firebase/AdMob'
+
+  target 'CostiMateTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
